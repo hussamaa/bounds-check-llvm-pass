1d70031e6a2cf7cd9e9978510c1f776de5701186
Hussama Ismail <hussama.ismail@gmail.com>
2019-12-04 11:33:21 +0000

    adding into the regression forced runtime checks

--- run-regression.sh
+++ run-regression.sh
@@ -3,36 +3,42 @@
 TESTS_FOLDER=./regression/static
 TEST_FILES="$(ls $TESTS_FOLDER)"
 
-BYTECODE_PLAIN_FOLDER=./build/bytecode/plain
-BYTECODE_TRANSFORMED_FOLDER=./bytecode/transformed
-BYTECODE_TRANSFORMED_OPTIMIZED_FOLDER=./bytecode/transformed+optimized
-mkdir -p $BYTECODE_PLAIN_FOLDER $BYTECODE_TRANSFORMED_FOLDER $BYTECODE_TRANSFORMED_OPTIMIZED_FOLDER
+BYTECODE_FOLDER=./build/bytecode/
+rm -rf $BYTECODE_FOLDER
+mkdir -p $BYTECODE_FOLDER
 
 BYTECODE_GENERATION_COMMAND="clang -S -emit-llvm "
 OPT_PASS_COMMAND="opt -load build/libLLVMBoundsCheck.so -bounds-check "
 
-expect_runtime=0;
 echo "****** regression tests ******"
 echo ""
 
 for tc in $TEST_FILES; do
   echo -n "$tc"
   is_fail=$(echo $tc | grep "FAIL" | wc -l)
-  $($BYTECODE_GENERATION_COMMAND $TESTS_FOLDER/$tc -o $BYTECODE_PLAIN_FOLDER/$tc.ll 2> /dev/null)
-  $($OPT_PASS_COMMAND $BYTECODE_PLAIN_FOLDER/$tc.ll > /dev/null 2> /dev/null)
+  $($BYTECODE_GENERATION_COMMAND $TESTS_FOLDER/$tc -o $BYTECODE_FOLDER/$tc.bc 2> /dev/null)
+  $($OPT_PASS_COMMAND $BYTECODE_FOLDER/$tc.bc > /dev/null 2> /dev/null)
   pass_error_level=$?
-  if [ $is_fail -eq   $pass_error_level ]; then
+  if [ $is_fail -eq $pass_error_level ]; then
     echo " [OK]"
   else
     echo " [FAIL]"
   fi  
-  #echo "Generating bytecode transformed (with bounds checks)..."
-  #$($BYTECODE_GENERATION_COMMAND $TESTS_FOLDER/$tc -o $BYTECODE_TRANSFORMED_FOLDER/$tc.ll 2> /dev/null)
-  #echo "Generating bytecode transformed and optimized..."
-  #$($BYTECODE_GENERATION_COMMAND $TESTS_FOLDER/$tc -o $BYTECODE_TRANSFORMED_OPTIMIZED_FOLDER/$tc.ll 2> /dev/null)
-  #echo "[OK]"
-  #echo ""
+  echo -n "$tc (with runtime check)"
+  $($OPT_PASS_COMMAND $BYTECODE_FOLDER/$tc.bc --force-runtime-checks -o $BYTECODE_FOLDER/$tc.ll > /dev/null 2> /dev/null)
+  $(llc -march=x86-64 -relocation-model=pic $BYTECODE_FOLDER/$tc.ll -o $BYTECODE_FOLDER/$tc.s)
+  $(gcc $BYTECODE_FOLDER/$tc.s -o $BYTECODE_FOLDER/$tc.exe)
+  ./$BYTECODE_FOLDER/$tc.exe > /dev/null 2> /dev/null
+  runtime_error_level=$?
+  if [ $is_fail -eq 0 ] && [ $runtime_error_level -eq 0 ]; then
+    echo " [OK]"
+  elif [ $is_fail -eq 1 ] && [ $runtime_error_level -eq 134 ]; then
+    echo " [OK]"
+  else
+    echo " [FAIL]"
+  fi  
 done
+
 echo " "
 echo "Date: \n$(date)"
 echo "******************************"
